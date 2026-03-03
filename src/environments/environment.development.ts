58047b89805d4c4f4ff85b88dce58bbb01168f92
TiavinaRavaka <99461072+Tiavina23@users.noreply.github.com>
2026-03-03 22:48:09 +0300

    Update API URL for development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://m1p13mean-backend.onrender.com',
+    apiUrl: 'https://m1p13mean-heninkaja-ravaka-backend.onrender.com',
 };
